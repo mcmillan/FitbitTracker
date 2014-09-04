0bc38a5572ac03563462754a9b6571d5df61bc51
Josh McMillan <josh@joshmcmillan.co.uk>
2014-09-04 01:10:07 +0100

    Initial Commit

---
+++ FitbitTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FitbitTracker
+//
+//  Created by Josh McMillan on 04/09/2014.
+//  Copyright (c) 2014 Chainsaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
