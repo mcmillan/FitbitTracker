4a6c48d48fa99de1acc4573daf2e15944a649216
Josh McMillan <josh@joshmcmillan.co.uk>
2014-09-04 01:30:12 +0100

    Getting BLE up and running

--- FitbitTracker/ViewController.swift
+++ FitbitTracker/ViewController.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
-                            
+class ViewController: UIViewController, CBCentralManagerDelegate {
+    
+    var centralManager: CBCentralManager!
+    
     override func viewDidLoad() {
+        centralManager = CBCentralManager(delegate: self, queue: nil)
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func centralManagerDidUpdateState(central: CBCentralManager!) {
+        println("Central manager has updated state - how exciting.")
+        
+        switch(central.state) {
+            case .PoweredOff:
+                println("BLE turned off.")
+            
+            case .PoweredOn:
+                println("BLE turned on.")
+            
+            case .Resetting:
+                println("BLE hardware is resetting.")
+            
+            case .Unauthorized:
+                println("Not authorized for BLE access.")
+        
+            case .Unknown:
+                println("BLE status is unknown.")
+            
+            case .Unsupported:
+                println("BLE is unsupported on this platform")
+        }
+    }
 
 }
 
